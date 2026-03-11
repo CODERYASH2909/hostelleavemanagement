c79569532c68debb9fae51b70a95263d71591780
devsmita <devsmitasahoo3@gmail.com>
2026-03-11 08:47:30 +0530

    Update settings and add gitignore

--- leaveform/settings.py
+++ leaveform/settings.py
@@ -78,17 +78,17 @@ WSGI_APPLICATION = 'leaveform.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lf_db',
+        'NAME': 'leaveform',
         'USER': 'root',
         'PASSWORD': '3029',
         'HOST': 'localhost',
         'PORT': '3306',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
